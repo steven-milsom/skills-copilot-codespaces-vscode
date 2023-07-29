583317b507c6e1d5e4fdc49675a3ae875a4dc5cc
steven-milsom <140872104+steven-milsom@users.noreply.github.com>
2023-07-29 10:31:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+    var memberMember = document.getElementById("memberMember");
+    skills.style.display = "block";
+    member.style.display = "none";
+    skillsMember.style.display = "block";
+    memberMember.style.display = "none";
+}
